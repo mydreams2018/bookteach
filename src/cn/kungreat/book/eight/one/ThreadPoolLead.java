cc5e1492739b724430ecf7c3b49f5d05fc1aa443
kungreat <mydreams2018@outlook.com>
2023-01-02 21:22:19 +0800

    第8章 ThreadPoolLead -1

--- src/cn/kungreat/book/eight/one/ThreadPoolLead.java
+++ src/cn/kungreat/book/eight/one/ThreadPoolLead.java
@@ -14,11 +14,15 @@ public class ThreadPoolLead {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        new Thread(new LoopRun(),"A").start();
+        LoopRun loopRun = new LoopRun();
+        new Thread(loopRun,"A").start();
+        new Thread(loopRun,"B").start();
         QueueRun one = new QueueRun("one");
         BLOCKING_QUEUE.add(one);
         BLOCKING_QUEUE.add(new QueueRun("two"));
         BLOCKING_QUEUE.add(new QueueRun("three"));
+        BLOCKING_QUEUE.add(new QueueRun("four"));
+
         System.out.println(one.getResult());
         System.out.println("main-end");
     }
@@ -27,14 +31,14 @@ public class ThreadPoolLead {
 
         @Override
         public void run() {
-                try {
-                    while (true){
-                        QueueRun take = BLOCKING_QUEUE.take();
-                        take.run();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            try {
+                while (true){
+                    QueueRun take = BLOCKING_QUEUE.take();
+                    take.run();
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
     }
@@ -43,9 +47,10 @@ public class ThreadPoolLead {
 
         private String name;
 
-        private Integer rt;
+        private Integer result;
 
         private Lock lock = new ReentrantLock();
+
         private Condition condition = lock.newCondition();
 
         public QueueRun(String name) {
@@ -58,9 +63,9 @@ public class ThreadPoolLead {
             try {
                 Thread.sleep(RANDOM.nextInt(5000));
                 System.out.println(Thread.currentThread().getName()+":"+this.name);
-                rt = RANDOM.nextInt(10000);
+                result = RANDOM.nextInt(666666);
             } catch (InterruptedException e) {
-                rt = -1;
+                result = -1;
                 e.printStackTrace();
             } finally {
                 condition.signalAll();
@@ -71,16 +76,17 @@ public class ThreadPoolLead {
         public Integer getResult(){
             lock.lock();
             try {
-                while (rt == null){
-                    condition.await();
-                }
-                return rt;
+              while (result == null){
+                  condition.await();
+              }
+              return result;
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            } finally {
+            }finally {
                 lock.unlock();
             }
             return null;
         }
     }
+
 }
