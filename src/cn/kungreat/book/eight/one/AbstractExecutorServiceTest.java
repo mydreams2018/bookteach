123ef2b33a3d872d76a7f01889c0f8d37d65fd4f
kungreat <mydreams2018@outlook.com>
2023-01-10 23:37:36 +0800

    第8章 FutureTask

---
+++ src/cn/kungreat/book/eight/one/AbstractExecutorServiceTest.java
@@ -0,0 +1,8 @@
+package cn.kungreat.book.eight.one;
+
+public class AbstractExecutorServiceTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/cn/kungreat/book/eight/one/FutureTaskTest.java
@@ -0,0 +1,41 @@
+package cn.kungreat.book.eight.one;
+
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+public class FutureTaskTest {
+
+    private static final FutureTask<Integer> FUTURE_TASK = new FutureTask<>(new RunnableImpl(),100);
+
+    public static void main(String[] args) throws Exception {
+        new Thread(FUTURE_TASK,"A").start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(FUTURE_TASK.get());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"B").start();
+
+        Thread.sleep(500);
+        FUTURE_TASK.cancel(true);
+        System.out.println("main-end");
+    }
+
+    static class RunnableImpl implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000);
+                System.out.println("RunnableImpl:"+Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
