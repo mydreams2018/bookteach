f73955ccd81efe9e82290ac9172781be8fe55225
kungreat <mydreams2018@outlook.com>
2023-01-25 14:21:28 +0800

    第8章 ScheduledThreadTest 02

--- src/cn/kungreat/book/eight/four/ScheduledThreadTest.java
+++ src/cn/kungreat/book/eight/four/ScheduledThreadTest.java
@@ -10,13 +10,14 @@ public class ScheduledThreadTest {
             new ScheduledThreadPoolExecutor(2);
 
     public static void main(String[] args) {
-        SCHEDULED_POOL.schedule(new Callable<Object>() {
-            @Override
-            public Object call() throws Exception {
-                System.out.println(Thread.currentThread().getName());
-                return null;
-            }
-        }, 5, TimeUnit.SECONDS);
+//        SCHEDULED_POOL.scheduleAtFixedRate(new RunnableImpl(), 5,2, TimeUnit.SECONDS);
+
+        //scheduleAtFixedRate   1000  1005  1007 1009 1011
+
+//        SCHEDULED_POOL.scheduleWithFixedDelay(new RunnableImpl(), 5,2, TimeUnit.SECONDS);
+        //scheduleWithFixedDelay   1000  1005+3 1010+3 1015+3
+
+        SCHEDULED_POOL.submit(new RunnableImpl());
     }
 
     static class RunnableImpl implements Runnable{
@@ -24,8 +25,8 @@ public class ScheduledThreadTest {
         @Override
         public void run() {
             try {
-//                Thread.sleep(5000);
-                System.out.println(Thread.currentThread().getName());
+                Thread.sleep(3000);
+                System.out.println(System.currentTimeMillis());
             } catch (Exception e) {
                 e.printStackTrace();
             }
