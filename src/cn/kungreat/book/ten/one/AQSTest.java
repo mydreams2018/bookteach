a920e521a69387c6f6e063789bd364f65d8d6593
kungreat <mydreams2018@outlook.com>
2023-02-09 22:03:37 +0800

    10 AQS 02

--- src/cn/kungreat/book/ten/one/AQSTest.java
+++ src/cn/kungreat/book/ten/one/AQSTest.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 public class AQSTest {
     private static final Sync SYNC = new Sync();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -26,9 +26,10 @@ public class AQSTest {
         SYNC.acquire(1);
         try {
             Thread.sleep(1000);
-            thread.interrupt();
-            System.out.println(Thread.currentThread().getName());
-        }finally {
+//            thread.interrupt();
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
             SYNC.release(1);
         }
         System.out.println("main-end");
@@ -36,24 +37,23 @@ public class AQSTest {
 
     private static class Sync extends AbstractQueuedSynchronizer {
 
-        public Sync() {
-//            setState(-1);
-        }
-
         //加锁时回调此方法
         public boolean tryAcquire(int acquires) {
             if (compareAndSetState(0, 1)) {
                 setExclusiveOwnerThread(Thread.currentThread());
+                System.out.println(Thread.currentThread().getName()+"getLock");
                 return true;
             }
             return false;
         }
+
         //释放锁时回调此方法
         protected boolean tryRelease(int releases) {
             if (!isHeldExclusively())
                 throw new IllegalMonitorStateException();
             setExclusiveOwnerThread(null);
             setState(0);
+            System.out.println(Thread.currentThread().getName()+"releaseLock");
             return true;
         }
         //是否是当前执行线程对象拥有此锁
